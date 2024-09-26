95c830e2ba7026f72e226f67884cfcb49afcf4d1
Sachin Mugwane <124877267+sachinmugwane@users.noreply.github.com>
2024-09-26 14:49:20 +0530

    Add files via upload

---
+++ StorageAccount_Used_Capacity.ps1
@@ -0,0 +1,17 @@
+﻿$sub = Get-AzSubscription | select Name  
+$sub | foreach {   
+Set-AzContext -Subscription $_.Name  
+$currentSub = $_.Name  
+$RGs = Get-AzResourceGroup | select ResourceGroupName  
+$RGs | foreach {  
+$CurrentRG = $_.ResourceGroupName  
+$StorageAccounts = Get-AzStorageAccount -ResourceGroupName $CurrentRG | select StorageAccountName  
+$StorageAccounts | foreach {  
+$StorageAccount = $_.StorageAccountName  
+$CurrentSAID = (Get-AzStorageAccount -ResourceGroupName $CurrentRG -AccountName $StorageAccount).Id  
+$usedCapacity = (Get-AzMetric -ResourceId $CurrentSAID -MetricName "UsedCapacity").Data  
+$usedCapacityInMB = $usedCapacity.Average / 1024 / 1024  
+"$StorageAccount,$usedCapacityInMB,$CurrentRG,$currentSub" >> ".\storageAccountsUsedCapacity.csv"  
+}  
+}  
+}
